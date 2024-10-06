6ad34d6cb6dca42b9a577095a321fc5bb443b767
maxpe <maxpe@Y0RUICHI>
2024-10-06 12:24:02 +0900

    Добавьте файлы проекта.

---
+++ pr2kalkulator/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr2kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Обработчик для всех кнопок (общий)
+        private void buttonClick(object sender, EventArgs e)
+        {
+            var currentButton = sender as Button;
+            textBox1.Text += currentButton.Text;
+        }
+
+        // Обработчик для button11
+        private void button11_Click(object sender, EventArgs e)
+        {
+            var d = new DataTable();
+            textBox1.Text = d.Compute(textBox1.Text, "").ToString();
+        }
+
+        // Обработчик для button13
+        private void button13_Click(object sender, EventArgs e)
+        {
+            var str = "";
+            for (int i = 0; i < textBox1.Text.Length - 1; i++)
+            {
+                str += textBox1.Text[i];
+            }
+            textBox1.Text = str;
+        }
+
+        // Обработчик для button12 (очистка текстового поля)
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+    }
+}
